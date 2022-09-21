90e65d2dd5a17dab61e04608b9fdf3512b966e4c
Jason Otter <jasonotter17@gmail.com>
2022-09-21 09:25:14 -0700

    add loading gif functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Container, Row, Col, Table, Pagination } from "react-bootstrap";
 import SearchInput from "./components/SearchInput";
 import "./App.css";
+import { ThreeCircles } from "react-loader-spinner";
 
 const tableCols = {
   people: ["Name", "Birthdate", "Height", "Mass", "Species", "Homeworld"],
@@ -26,6 +27,7 @@ const client = axios.create({
 
 const App = () => {
   const [tableType, setTableType] = useState(tableCols.people);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getData = async (path) => {
     const data = await client.get(path);
@@ -34,44 +36,57 @@ const App = () => {
 
   const searchQuerySubmitHandler = async (category, queryStr) => {
     const path = `${category}/?search=${encodeURIComponent(queryStr)}`;
+    setIsLoading(true);
     const data = await getData(path);
-
+    setIsLoading(false);
     console.log(data);
-
     setTableType(tableCols[category]);
   };
 
   return (
-    <main>
-      <Container>
-        <Row>
-          <Col>
-            <header>
-              <h1>Star Wars API</h1>
-              <span>Built with React.js</span>
-            </header>
-            <section>
-              <SearchInput onSearchQuerySubmit={searchQuerySubmitHandler} />
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    {tableType.map((col) => {
-                      return <th key={col}>{col}</th>;
-                    })}
-                  </tr>
-                </thead>
-                <tbody>{/* conditional rows */}</tbody>
-              </Table>
-              <Pagination>
-                <Pagination.Prev />
-                <Pagination.Item>{1}</Pagination.Item>
-                <Pagination.Next />
-              </Pagination>
-            </section>
-          </Col>
-        </Row>
-      </Container>
-    </main>
+    <>
+      <main>
+        <Container>
+          <Row>
+            <Col>
+              <header>
+                <h1>Star Wars API</h1>
+                <span>Built with React.js</span>
+              </header>
+              <section>
+                <SearchInput onSearchQuerySubmit={searchQuerySubmitHandler} />
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      {tableType.map((col) => {
+                        return <th key={col}>{col}</th>;
+                      })}
+                    </tr>
+                  </thead>
+                  <tbody>{/* conditional rows */}</tbody>
+                </Table>
+                <Pagination>
+                  <Pagination.Prev />
+                  <Pagination.Item>{1}</Pagination.Item>
+                  <Pagination.Next />
+                </Pagination>
+              </section>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <div className={isLoading ? "overlay overlay-on" : "overlay"}>
+        <ThreeCircles
+          height="100"
+          width="100"
+          color="#2C8BCE"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={isLoading}
+          ariaLabel="three-circles-rotating"
+        />
+      </div>
+    </>
   );
 };
 
